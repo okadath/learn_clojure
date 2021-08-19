b8b71500f3d9a935ce0efd6b5e57625c6a28fd83
okadath <vintaw.01@gmail.com>
2021-08-19 11:35:55 -0500

    hashs & lists

--- history_clean.clj
+++ history_clean.clj
@@ -33,3 +33,49 @@
 (chatty-average 10,20)
 (+ 8 8)
 ; (load-file "repl-history.clj")
+(vector true 2 "asd " 5)
+(vector)
+(def novels ["emma" "coma" "war and peace"])
+(first novels )
+(rest novels)
+; (rest(rest(novels))
+; )
+(rest(rest novels ))
+(novels 2)
+(novels 0)
+(nth novels 2)
+(nth novels 1)
+(conj novels "carrie")
+(cons novels "a")
+(cons "a " novels )
+'(1 2 3)
+(hash-map "title" "Oliver Twist"
+"author" "Dickens"
+"published" 1838)
+(def book
+(hash-map "title" "Oliver Twist"
+"author" "Dickens"
+"published" 1838)
+)
+book
+(get book "title")
+(book "title")
+(def book {:title "oliver" :author "dickens" :pub 1838})
+(println "title " (book :title))
+(println "title " (:title book))
+(assoc book :page-count 362 :title "War & Peace")
+book
+; (b (assoc book :page-count 362 :title "War & Peace"))
+(def b (assoc book :page-count 362 :title "War & Peace"))
+b
+(def c (dissoc book :page-count 362 ))
+c
+(def c (dissoc book :pub ))
+c
+(:keys book)
+(:keys c)
+(keys c)
+(keys book)
+(vals book)
+(def authors #{"Dickens" "Austen" "King"})
+authors 
--- readme.md
+++ readme.md
@@ -18,8 +18,21 @@ las comas las trata como espacios :O
 
 
 cargar scripts de clojure y ejecutarlos en consola 
-```clj
+```bash
+$ 
+lein repl
+
+=>
 (load-file "history_clean.clj")
 ```
 
 el repl-history es todo el de lein asi que hay que copiarlo(x q se recarga) y limpiarlo de errores antes de cargarlo
+
+la ```'``` significa que la siguiente lista son datos (= "no evalues clojure pls!!")
+
+todo se vuelve a copiar, arrays o cvectores o mapas, si es mas facil se usa []
+hash-map se usa para {}
+
+assoc y dissoc agregan elementos al {} y funcionan con vectores
+
+los keywords son como strings pero apra indicar nombres :key
